2f5146eed60145ae17cc241d03bf68781217ceb9
Patrick Van Weelden <pvanweel@yahoo.com>
2024-02-16 15:04:39 -0600

    added one to many relationship between user and reports

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Report } from './report.entity';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { User } from 'src/users/user.entity';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
+
+  async create(reportDto: CreateReportDto, user: User) {
+    const report = this.repo.create(reportDto);
+    // report.userId = user.id;
+    report.user = user;
+
+    return await this.repo.save(report);
+  }
+}
